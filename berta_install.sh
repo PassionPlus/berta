8a5acd90ca830cded93990385879df74accd83de
stella <stella@stella.de>
2021-04-11 13:55:37 +0200

    Created and added berta_install script

---
+++ berta_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This is the install script
+# TODO More info
+
+# ensure user is running as root
+if [ "$EUID" -ne 0 ]
+	then echo "ERROR: Script must be run as root. Exiting..."
+	exit
+fi
+
+echo "Test, running as root"
+
+# TODO Update and upgrade raspberry
+# TODO Download microphone script
+# TODO Run microphone script
+# TODO install needed programms and dependancies
+# TODO Set up python environment
+# TODO ... Profit$?

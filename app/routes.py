a7748875414474491d014934c33a58970c7b1913
Stella Neser <1610286@stud.hs-mannheim.de>
2021-05-25 13:36:14 +0200

    add templates, add mock data

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Admin'}
+    deepspeechlogs = [
+        {
+            'date': '25.05.2020',
+            'body': 'Turn on lights'
+        },
+        {
+            'date': '26.05.2020',
+            'body': 'Tell joke'
+        }    
+    ]
+    return render_template('index.html', title='Home', user=user, deepspeechlogs=deepspeechlogs)

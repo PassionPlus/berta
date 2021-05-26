d061215638a78c93cdfb352b5cb997ad53728db5
Stella Neser <1610286@stud.hs-mannheim.de>
2021-05-26 10:03:33 +0200

    add semi done login front

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from flask.helpers import url_for
+from app.forms import LoginForm
+from flask import render_template, flash, redirect
 from app import app
 
 @app.route('/')
@@ -16,3 +18,12 @@ def index():
         }    
     ]
     return render_template('index.html', title='Home', user=user, deepspeechlogs=deepspeechlogs)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for user {}, remeber_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)

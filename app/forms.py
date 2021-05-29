7f1ea5f35c14ed1fa74657da58c4ea913162f2ea
Stella Neser <1610286@stud.hs-mannheim.de>
2021-05-29 11:09:48 +0200

    add user profile page with edit function

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,16 @@
 # forms holds the used forms
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Sumbit')
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,17 @@
+from datetime import datetime
 from flask_login import current_user, login_user, logout_user, login_required
+from flask_migrate import current
 from werkzeug.wrappers import request
 from werkzeug.urls import url_parse
 from flask.helpers import url_for
 from flask import render_template, flash, redirect, request
 from app.models import User
-from app.forms import LoginForm
-from app import app
+from app.forms import LoginForm, EditProfileForm
+from app import app, db
 
 @app.route('/')
 @app.route('/index')
-@login_required         # only logged useres can visit the above routes
+@login_required         # decorator, only logged useres can visit the above routes
 def index():
     deepspeechlogs = [{
             'timestamp': '25',
@@ -47,3 +49,34 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/user/<username>')      # route has a dynamic component in it
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
+
+# Decorator function, executed right befor the view function.
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:                   # if the user is logged in, than set the last_seen fielt to curent time
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+# View function
+@app.route('/edit_profile', methods=['GET','POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():                       # if validation is true copy the data form into the user object and then write it to the database
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':                       # request by the first time
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)

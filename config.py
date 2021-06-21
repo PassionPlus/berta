fa3f4f772be8495664ad1adeee76624228baccd0
respeaker <respeaker@seeed.cc>
2021-06-21 18:01:01 +0200

    - Changed config.py to only display a few items from db - finished first version of install script - added ability to test speech command in routes.py  Zum Commit vorgemerkte Änderungen: 	geändert:       app/routes.py 	geändert:       config.py 	umbenannt:      berta_runme_first.sh -> install.sh

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'should-be-changed'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'should-be-changed'    # config item against CSFR attacks
+    # config item to change the database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTS_PER_PAGE = 20                 # config item to that determines how many items will be displayer per page
+    SQLALCHEMY_TRACK_MODIFICATIONS = False                              # Connect to the following signals to get notified before and after changes are committed to the database. These changes are only tracked if SQLALCHEMY_TRACK_MODIFICATIONS is enabled in the config.
+    POSTS_PER_PAGE = 5                                                  # config item to that determines how many items will be displayer per page
